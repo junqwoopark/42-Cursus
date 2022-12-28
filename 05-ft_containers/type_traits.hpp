1206ee0c251aaccea431f9ab020871d9e30b9166
junqwoopark <junqwoopark@gmail.com>
2022-12-29 01:24:11 +0900

    std::is_integral c++11 자료형 삭제

--- 05-ft_containers/type_traits.hpp
+++ 05-ft_containers/type_traits.hpp
@@ -9,35 +9,55 @@ namespace ft {
 ** enable_if
 */
 template <bool B, class T = void> struct enable_if {};
-template <class T> struct enable_if<true, T> { typedef T type; };
+template <class T> struct enable_if<true, T> {
+  typedef T type;
+};
 
 /*
 ** is_integral
 */
-template <class T> struct is_integral { static const bool value = false; };
-template <> struct is_integral<bool> { static const bool value = true; };
-template <> struct is_integral<char> { static const bool value = true; };
-template <> struct is_integral<signed char> { static const bool value = true; };
+template <class T> struct is_integral {
+  static const bool value = false;
+};
+template <> struct is_integral<bool> {
+  static const bool value = true;
+};
+template <> struct is_integral<char> {
+  static const bool value = true;
+};
+template <> struct is_integral<signed char> {
+  static const bool value = true;
+};
 template <> struct is_integral<unsigned char> {
   static const bool value = true;
 };
-template <> struct is_integral<wchar_t> { static const bool value = true; };
-template <> struct is_integral<short> { static const bool value = true; };
+template <> struct is_integral<wchar_t> {
+  static const bool value = true;
+};
+template <> struct is_integral<short> {
+  static const bool value = true;
+};
 template <> struct is_integral<unsigned short> {
   static const bool value = true;
 };
-template <> struct is_integral<int> { static const bool value = true; };
+template <> struct is_integral<int> {
+  static const bool value = true;
+};
 template <> struct is_integral<unsigned int> {
   static const bool value = true;
 };
-template <> struct is_integral<long> { static const bool value = true; };
+template <> struct is_integral<long> {
+  static const bool value = true;
+};
 template <> struct is_integral<unsigned long> {
   static const bool value = true;
 };
-template <> struct is_integral<long long> { static const bool value = true; };
-template <> struct is_integral<unsigned long long> {
-  static const bool value = true;
-};
+// template <> struct is_integral<long long> {
+//   static const bool value = true;
+// };
+// template <> struct is_integral<unsigned long long> {
+//   static const bool value = true;
+// };
 
 } // namespace ft
 
