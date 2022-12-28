65e85e8e4e357fc20dcbacb27fdb1c19a7ffa331
junqwoopark <junqwoopark@gmail.com>
2022-12-29 01:26:27 +0900

    std::reverse_iterator 수정 및 필요 없는 것 주석 처리

--- 05-ft_containers/iterator.hpp
+++ 05-ft_containers/iterator.hpp
@@ -3,32 +3,33 @@
 
 #include "type_traits.hpp"
 #include <cstddef>
+#include <iterator>
 
 namespace ft {
 
-/*
-** iterator_tag
-** iterator의 특성을 나타낸다.
-*/
-struct input_iterator_tag {};
-struct output_iterator_tag {};
-struct forward_iterator_tag : public input_iterator_tag {};
-struct bidirectional_iterator_tag : public forward_iterator_tag {};
-struct random_access_iterator_tag : public bidirectional_iterator_tag {};
+// /*
+// ** iterator_tag
+// ** iterator의 특성을 나타낸다.
+// */
+// struct inputIterator_tag {};
+// struct outputIterator_tag {};
+// struct forwardIterator_tag : public inputIterator_tag {};
+// struct bidirectionalIterator_tag : public forwardIterator_tag {};
+// struct random_accessIterator_tag : public bidirectionalIterator_tag {};
 
-/*
-** iterator
-** iterator의 특성을 정의한다.
-*/
-template <class Category, class T, class Distance = ptrdiff_t,
-          class Pointer = T *, class Reference = T &>
-struct iterator {
-  typedef Category iterator_category;
-  typedef T value_type;
-  typedef Distance difference_type;
-  typedef Pointer pointer;
-  typedef Reference reference;
-};
+// /*
+// ** iterator
+// ** iterator의 특성을 정의한다.
+// */
+// template <class Category, class T, class Distance = ptrdiff_t,
+//           class Pointer = T *, class Reference = T &>
+// struct iterator {
+//   typedef Category iterator_category;
+//   typedef T value_type;
+//   typedef Distance difference_type;
+//   typedef Pointer pointer;
+//   typedef Reference reference;
+// };
 
 /*
 ** iterator_traits
@@ -43,7 +44,7 @@ template <class Iterator> struct iterator_traits {
 };
 
 template <class T> struct iterator_traits<T *> {
-  typedef random_access_iterator_tag iterator_category;
+  typedef std::random_access_iterator_tag iterator_category;
   typedef T value_type;
   typedef ptrdiff_t difference_type;
   typedef T *pointer;
@@ -51,71 +52,12 @@ template <class T> struct iterator_traits<T *> {
 };
 
 template <class T> struct iterator_traits<const T *> {
-  typedef random_access_iterator_tag iterator_category;
+  typedef std::random_access_iterator_tag iterator_category;
   typedef T value_type;
   typedef ptrdiff_t difference_type;
   typedef const T *pointer;
   typedef const T &reference;
 };
-
-/*
-** distance
-** iterator의 거리를 구한다.
-*/
-template <class InputIterator>
-typename iterator_traits<InputIterator>::difference_type
-distance(InputIterator first, InputIterator last) {
-  typename iterator_traits<InputIterator>::difference_type n = 0;
-  while (first != last) {
-    ++first;
-    ++n;
-  }
-  return n;
-}
-
-template <class RandomAccessIterator>
-typename iterator_traits<RandomAccessIterator>::difference_type
-distance(RandomAccessIterator first, RandomAccessIterator last,
-         random_access_iterator_tag) {
-  return last - first;
-}
-
-template <class InputIterator>
-typename iterator_traits<InputIterator>::difference_type
-distance(InputIterator first, InputIterator last, input_iterator_tag) {
-  return distance(first, last,
-                  typename iterator_traits<InputIterator>::iterator_category());
-}
-
-/*
-** advance
-** iterator을 n만큼 이동시킨다.
-*/
-template <class InputIterator, class Distance>
-void advance(InputIterator &i, Distance n, input_iterator_tag) {
-  while (n--)
-    ++i;
-}
-
-template <class BidirectionalIterator, class Distance>
-void advance(BidirectionalIterator &i, Distance n, bidirectional_iterator_tag) {
-  if (n >= 0)
-    while (n--)
-      ++i;
-  else
-    while (n++)
-      --i;
-}
-
-template <class RandomAccessIterator, class Distance>
-void advance(RandomAccessIterator &i, Distance n, random_access_iterator_tag) {
-  i += n;
-}
-
-template <class InputIterator, class Distance>
-void advance(InputIterator &i, Distance n) {
-  advance(i, n, typename iterator_traits<InputIterator>::iterator_category());
-}
 
 /*
 ** reverse_iterator
@@ -126,73 +68,97 @@ protected:
   Iterator current;
 
 public:
-  typedef typename ft::iterator_traits<Iterator>::iterator_category
-      iterator_category;
-  typedef typename ft::iterator_traits<Iterator>::value_type value_type;
   typedef
-      typename ft::iterator_traits<Iterator>::difference_type difference_type;
-  typedef typename ft::iterator_traits<Iterator>::pointer pointer;
-  typedef typename ft::iterator_traits<Iterator>::reference reference;
+      typename iterator_traits<Iterator>::iterator_category iterator_category;
+  typedef typename iterator_traits<Iterator>::value_type value_type;
+  typedef typename iterator_traits<Iterator>::difference_type difference_type;
+  typedef typename iterator_traits<Iterator>::pointer pointer;
+  typedef typename iterator_traits<Iterator>::reference reference;
 
   typedef Iterator iterator_type;
-  typedef reverse_iterator<Iterator> self;
+  typedef reverse_iterator<Iterator> _Self;
 
 public:
   reverse_iterator() {}
   explicit reverse_iterator(iterator_type x) : current(x) {}
-  reverse_iterator(const self &x) : current(x.current) {}
+
+  reverse_iterator(const _Self &x) : current(x.current) {}
+  template <class _Iter>
+  reverse_iterator(const reverse_iterator<_Iter> &x) : current(x.base()) {}
 
   iterator_type base() const { return current; }
-
   reference operator*() const {
-    Iterator tmp = current;
-    return *--tmp;
+    Iterator __tmp = current;
+    return *--__tmp;
   }
   pointer operator->() const { return &(operator*()); }
 
-  self &operator++() {
+  _Self &operator++() {
     --current;
     return *this;
   }
-  self operator++(int) {
-    self tmp = *this;
+  _Self operator++(int) {
+    _Self __tmp = *this;
     --current;
-    return tmp;
+    return __tmp;
   }
-  self &operator--() {
+  _Self &operator--() {
     ++current;
     return *this;
   }
-  self operator--(int) {
-    self tmp = *this;
+  _Self operator--(int) {
+    _Self __tmp = *this;
     ++current;
-    return tmp;
+    return __tmp;
   }
 
-  self operator+(difference_type n) const { return self(current - n); }
-  self &operator+=(difference_type n) {
+  _Self operator+(difference_type n) const { return _Self(current - n); }
+  _Self &operator+=(difference_type n) {
     current -= n;
     return *this;
   }
-  self operator-(difference_type n) const { return self(current + n); }
-  self &operator-=(difference_type n) {
+  _Self operator-(difference_type n) const { return _Self(current + n); }
+  _Self &operator-=(difference_type n) {
     current += n;
     return *this;
   }
-
   reference operator[](difference_type n) const { return *(*this + n); }
 };
 
 template <class Iterator>
-inline bool operator==(const reverse_iterator<Iterator> &x,
-                       const reverse_iterator<Iterator> &y) {
+bool operator==(const reverse_iterator<Iterator> &x,
+                const reverse_iterator<Iterator> &y) {
   return x.base() == y.base();
 }
 
 template <class Iterator>
-inline bool operator<(const reverse_iterator<Iterator> &x,
-                      const reverse_iterator<Iterator> &y) {
+bool operator<(const reverse_iterator<Iterator> &x,
+               const reverse_iterator<Iterator> &y) {
   return y.base() < x.base();
+}
+
+template <class Iterator>
+bool operator!=(const reverse_iterator<Iterator> &x,
+                const reverse_iterator<Iterator> &y) {
+  return !(x == y);
+}
+
+template <class Iterator>
+bool operator>(const reverse_iterator<Iterator> &x,
+               const reverse_iterator<Iterator> &y) {
+  return y < x;
+}
+
+template <class Iterator>
+bool operator<=(const reverse_iterator<Iterator> &x,
+                const reverse_iterator<Iterator> &y) {
+  return !(y < x);
+}
+
+template <class Iterator>
+bool operator>=(const reverse_iterator<Iterator> &x,
+                const reverse_iterator<Iterator> &y) {
+  return !(x < y);
 }
 
 template <class Iterator>
@@ -207,18 +173,6 @@ inline reverse_iterator<Iterator>
 operator+(typename reverse_iterator<Iterator>::difference_type n,
           const reverse_iterator<Iterator> &x) {
   return reverse_iterator<Iterator>(x.base() - n);
-}
-
-template <class Iterator>
-inline bool operator!=(const reverse_iterator<Iterator> &x,
-                       const reverse_iterator<Iterator> &y) {
-  return !(x == y);
-}
-
-template <class Iterator>
-inline bool operator>(const reverse_iterator<Iterator> &x,
-                      const reverse_iterator<Iterator> &y) {
-  return y < x;
 }
 
 } // namespace ft
