55102dfa6f30faae0f727c40078f299aa3fbcca8
Junkyu Park <junqwoopark@gmail.com>
2022-12-31 16:30:34 +0900

    std::lexicographical_compare 추가

--- 05-ft_containers/algorithm.hpp
+++ 05-ft_containers/algorithm.hpp
@@ -31,6 +31,38 @@ bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2,
   return true;
 }
 
+/*
+** lexicographical_compare
+*/
+template <class InputIterator1, class InputIterator2>
+bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
+                             InputIterator2 first2, InputIterator2 last2) {
+  while (first1 != last1) {
+    if (first2 == last2 || *first2 < *first1)
+      return false;
+    else if (*first1 < *first2)
+      return true;
+    ++first1;
+    ++first2;
+  }
+  return (first2 != last2);
+}
+
+template <class InputIterator1, class InputIterator2, class Compare>
+bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
+                             InputIterator2 first2, InputIterator2 last2,
+                             Compare comp) {
+  while (first1 != last1) {
+    if (first2 == last2 || comp(*first2, *first1))
+      return false;
+    else if (comp(*first1, *first2))
+      return true;
+    ++first1;
+    ++first2;
+  }
+  return (first2 != last2);
+}
+
 } // namespace ft
 
 #endif
