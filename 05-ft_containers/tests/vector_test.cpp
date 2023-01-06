88b4c52e76ca4a91088d15eff482b123bad0422a
Junkyu Park <junqwoopark@gmail.com>
2023-01-06 15:55:21 +0900

    vector 테스트 파일 수정

--- 05-ft_containers/tests/vector_test.cpp
+++ 05-ft_containers/tests/vector_test.cpp
@@ -100,7 +100,7 @@ int main() {
     myvector.resize(12);
 
     std::cout << "myvector contains:";
-    for (int i = 0; i < myvector.size(); i++)
+    for (unsigned int i = 0; i < myvector.size(); i++)
       std::cout << ' ' << myvector[i];
     std::cout << '\n';
   }
