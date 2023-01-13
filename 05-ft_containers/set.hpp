870b6ec2c566cae9cb778cb31b6c83cfbfb2307c
junqwoopark <junqwoopark@gmail.com>
2023-01-13 21:51:32 +0900

    std::set 리팩토링

--- 05-ft_containers/set.hpp
+++ 05-ft_containers/set.hpp
@@ -21,102 +21,113 @@ class set {
   typedef Key value_type;
   typedef Compare key_compare;
   typedef Compare value_compare;
+  typedef Alloc allocator_type;
 
  private:
   typedef Rb_tree<key_type, value_type, Identity<value_type>, key_compare, Alloc> _Rep_type;
-  _Rep_type _M_t;
+  _Rep_type t;
 
  public:
   typedef typename _Rep_type::const_pointer pointer;
   typedef typename _Rep_type::const_pointer const_pointer;
   typedef typename _Rep_type::const_reference reference;
   typedef typename _Rep_type::const_reference const_reference;
+
   typedef typename _Rep_type::const_iterator iterator;
   typedef typename _Rep_type::const_iterator const_iterator;
   typedef typename _Rep_type::const_reverse_iterator reverse_iterator;
   typedef typename _Rep_type::const_reverse_iterator const_reverse_iterator;
+
   typedef typename _Rep_type::size_type size_type;
   typedef typename _Rep_type::difference_type difference_type;
-  typedef typename _Rep_type::allocator_type allocator_type;
 
-  // allocation/deallocation
-  set() : _M_t(Compare(), allocator_type()) {}
-  explicit set(const Compare &comp, const allocator_type &a = allocator_type()) : _M_t(comp, a) {}
+  // construct/copy/destroy:
+  set() : t(Compare(), allocator_type()) {}
+  explicit set(const Compare &comp, const allocator_type &a = allocator_type()) : t(comp, a) {}
 
   template <class InputIterator>
-  set(InputIterator first, InputIterator last) : _M_t(Compare(), allocator_type()) {
-    _M_t.insert_unique(first, last);
+  set(InputIterator first, InputIterator last) : t(Compare(), allocator_type()) {
+    t.insert_unique(first, last);
   }
   template <class InputIterator>
   set(InputIterator first, InputIterator last, const Compare &comp, const allocator_type &a = allocator_type())
-      : _M_t(comp, a) {
-    _M_t.insert_unique(first, last);
+      : t(comp, a) {
+    t.insert_unique(first, last);
   }
-  set(const set<Key, Compare, Alloc> &x) : _M_t(x._M_t) {}
+  set(const set &x) : t(x.t) {}
+  ~set() {}
 
-  set<Key, Compare, Alloc> &operator=(const set<Key, Compare, Alloc> &x) {
-    _M_t = x._M_t;
+  set &operator=(const set &x) {
+    t = x.t;
     return *this;
   }
 
-  // accessors:
-  key_compare key_comp() const { return _M_t.key_comp(); }
-  value_compare value_comp() const { return _M_t.key_comp(); }
-  allocator_type get_allocator() const { return _M_t.get_allocator(); }
+  /*
+   * iterator
+   */
+  iterator begin() { return t.begin(); }
+  const_iterator begin() const { return t.begin(); }
+  iterator end() { return t.end(); }
+  const_iterator end() const { return t.end(); }
 
-  iterator begin() { return _M_t.begin(); }
-  const_iterator begin() const { return _M_t.begin(); }
-  iterator end() { return _M_t.end(); }
-  const_iterator end() const { return _M_t.end(); }
-  reverse_iterator rbegin() { return _M_t.rbegin(); }
-  const_reverse_iterator rbegin() const { return _M_t.rbegin(); }
-  reverse_iterator rend() { return _M_t.rend(); }
-  const_reverse_iterator rend() const { return _M_t.rend(); }
-  bool empty() const { return _M_t.empty(); }
-  size_type size() const { return _M_t.size(); }
-  size_type max_size() const { return _M_t.max_size(); }
+  reverse_iterator rbegin() { return t.rbegin(); }
+  const_reverse_iterator rbegin() const { return t.rbegin(); }
+  reverse_iterator rend() { return t.rend(); }
+  const_reverse_iterator rend() const { return t.rend(); }
 
-  void swap(set<Key, Compare, Alloc> &x) { _M_t.swap(x._M_t); }
+  /*
+   * capacity
+   */
+  bool empty() const { return t.empty(); }
+  size_type size() const { return t.size(); }
+  size_type max_size() const { return t.max_size(); }
 
-  // insert/erase
+  void swap(set<Key, Compare, Alloc> &x) { t.swap(x.t); }
 
-  pair<iterator, bool> insert(const value_type &x) { return _M_t.insert_unique(x); }
-  iterator insert(iterator position, const value_type &x) {
+  /*
+   * modifiers
+   */
+  pair<iterator, bool> insert(const value_type &x) { return t.insert_unique(x); }
+  iterator insert(const_iterator position, const value_type &x) {
     typedef typename _Rep_type::iterator _Rep_iterator;
-    return _M_t.insert_unique((_Rep_iterator &)position, x);
+    return t.insert_unique((_Rep_iterator &)position, x);
   }
   template <class InputIterator>
   void insert(InputIterator first, InputIterator last) {
-    _M_t.insert_unique(first, last);
+    t.insert_unique(first, last);
   }
-  template <class InputIterator>
-  void insert(InputIterator first, InputIterator last, iterator position) {
-    _M_t.insert_unique(first, last, position);
-  }
-  void erase(iterator position) {
+
+  void erase(const_iterator position) {
     typedef typename _Rep_type::iterator _Rep_iterator;
-    _M_t.erase((_Rep_iterator &)position);
+    t.erase((_Rep_iterator &)position);
   }
-  size_type erase(const key_type &x) { return _M_t.erase(x); }
+  size_type erase(const key_type &x) { return t.erase(x); }
   void erase(iterator first, iterator last) {
     typedef typename _Rep_type::iterator _Rep_iterator;
-    _M_t.erase((_Rep_iterator &)first, (_Rep_iterator &)last);
+    t.erase((_Rep_iterator &)first, (_Rep_iterator &)last);
   }
-  void clear() { _M_t.clear(); }
+  void clear() { t.clear(); }
 
-  // set operations:
-  iterator find(const key_type &x) { return _M_t.find(x); }
-  const_iterator find(const key_type &x) const { return _M_t.find(x); }
-  size_type count(const key_type &x) const { return _M_t.count(x); }
-  iterator lower_bound(const key_type &x) { return _M_t.lower_bound(x); }
-  const_iterator lower_bound(const key_type &x) const { return _M_t.lower_bound(x); }
-  iterator upper_bound(const key_type &x) { return _M_t.upper_bound(x); }
-  const_iterator upper_bound(const key_type &x) const { return _M_t.upper_bound(x); }
-  pair<iterator, iterator> equal_range(const key_type &x) { return _M_t.equal_range(x); }
-  pair<const_iterator, const_iterator> equal_range(const key_type &x) const { return _M_t.equal_range(x); }
+  /*
+   * observers
+   */
+  allocator_type get_allocator() const { return t.get_allocator(); }
+  key_compare key_comp() const { return t.key_comp(); }
+  value_compare value_comp() const { return t.key_comp(); }
 
-  friend bool operator==(const set &x, const set &y) { return x._M_t == y._M_t; }
-  friend bool operator<(const set &x, const set &y) { return x._M_t < y._M_t; }
+  // set operations
+  iterator find(const key_type &x) { return t.find(x); }
+  const_iterator find(const key_type &x) const { return t.find(x); }
+  size_type count(const key_type &x) const { return t.count(x); }
+  iterator lower_bound(const key_type &x) { return t.lower_bound(x); }
+  const_iterator lower_bound(const key_type &x) const { return t.lower_bound(x); }
+  iterator upper_bound(const key_type &x) { return t.upper_bound(x); }
+  const_iterator upper_bound(const key_type &x) const { return t.upper_bound(x); }
+  pair<iterator, iterator> equal_range(const key_type &x) { return t.equal_range(x); }
+  pair<const_iterator, const_iterator> equal_range(const key_type &x) const { return t.equal_range(x); }
+
+  friend bool operator==(const set &x, const set &y) { return x.t == y.t; }
+  friend bool operator<(const set &x, const set &y) { return x.t < y.t; }
 };
 
 template <class Key, class Compare, class Alloc>
