7e422f527e78b3fc47b515ca37e8566ad2faf6e7
junqwoopark <junqwoopark@gmail.com>
2023-01-14 21:00:21 +0900

    주석 수정 및 _tree 변수명 수정

--- 05-ft_containers/set.hpp
+++ 05-ft_containers/set.hpp
@@ -24,110 +24,121 @@ class set {
   typedef Alloc allocator_type;
 
  private:
-  typedef Rb_tree<key_type, value_type, Identity<value_type>, key_compare, Alloc> _Rep_type;
-  _Rep_type t;
+  typedef Rb_tree<key_type, value_type, Identity<value_type>, key_compare, Alloc> _Tree_type;
+  _Tree_type _tree;
 
  public:
-  typedef typename _Rep_type::const_pointer pointer;
-  typedef typename _Rep_type::const_pointer const_pointer;
-  typedef typename _Rep_type::const_reference reference;
-  typedef typename _Rep_type::const_reference const_reference;
+  typedef typename _Tree_type::const_pointer pointer;
+  typedef typename _Tree_type::const_pointer const_pointer;
+  typedef typename _Tree_type::const_reference reference;
+  typedef typename _Tree_type::const_reference const_reference;
 
-  typedef typename _Rep_type::const_iterator iterator;
-  typedef typename _Rep_type::const_iterator const_iterator;
-  typedef typename _Rep_type::const_reverse_iterator reverse_iterator;
-  typedef typename _Rep_type::const_reverse_iterator const_reverse_iterator;
+  typedef typename _Tree_type::const_iterator iterator;
+  typedef typename _Tree_type::const_iterator const_iterator;
+  typedef typename _Tree_type::const_reverse_iterator reverse_iterator;
+  typedef typename _Tree_type::const_reverse_iterator const_reverse_iterator;
 
-  typedef typename _Rep_type::size_type size_type;
-  typedef typename _Rep_type::difference_type difference_type;
+  typedef typename _Tree_type::size_type size_type;
+  typedef typename _Tree_type::difference_type difference_type;
 
-  // construct/copy/destroy:
-  set() : t(Compare(), allocator_type()) {}
-  explicit set(const Compare &comp, const allocator_type &a = allocator_type()) : t(comp, a) {}
+  /*
+   * constructor
+   */
+  set() : _tree(Compare(), allocator_type()) {}
+  explicit set(const Compare &comp, const allocator_type &a = allocator_type()) : _tree(comp, a) {}
 
   template <class InputIterator>
-  set(InputIterator first, InputIterator last) : t(Compare(), allocator_type()) {
-    t.insert_unique(first, last);
+  set(InputIterator first, InputIterator last) : _tree(Compare(), allocator_type()) {
+    _tree.insert_unique(first, last);
   }
   template <class InputIterator>
   set(InputIterator first, InputIterator last, const Compare &comp, const allocator_type &a = allocator_type())
-      : t(comp, a) {
-    t.insert_unique(first, last);
+      : _tree(comp, a) {
+    _tree.insert_unique(first, last);
   }
-  set(const set &x) : t(x.t) {}
-  ~set() {}
+  set(const set &x) : _tree(x._tree) {}
 
+  /*
+   * assignment operator
+   */
   set &operator=(const set &x) {
-    t = x.t;
+    _tree = x._tree;
     return *this;
   }
 
   /*
-   * iterator
+   * destructor
    */
-  iterator begin() { return t.begin(); }
-  const_iterator begin() const { return t.begin(); }
-  iterator end() { return t.end(); }
-  const_iterator end() const { return t.end(); }
+  ~set() {}
 
-  reverse_iterator rbegin() { return t.rbegin(); }
-  const_reverse_iterator rbegin() const { return t.rbegin(); }
-  reverse_iterator rend() { return t.rend(); }
-  const_reverse_iterator rend() const { return t.rend(); }
+  /*
+   * iterators
+   */
+  iterator begin() { return _tree.begin(); }
+  const_iterator begin() const { return _tree.begin(); }
+  iterator end() { return _tree.end(); }
+  const_iterator end() const { return _tree.end(); }
+
+  reverse_iterator rbegin() { return _tree.rbegin(); }
+  const_reverse_iterator rbegin() const { return _tree.rbegin(); }
+  reverse_iterator rend() { return _tree.rend(); }
+  const_reverse_iterator rend() const { return _tree.rend(); }
 
   /*
    * capacity
    */
-  bool empty() const { return t.empty(); }
-  size_type size() const { return t.size(); }
-  size_type max_size() const { return t.max_size(); }
+  bool empty() const { return _tree.empty(); }
+  size_type size() const { return _tree.size(); }
+  size_type max_size() const { return _tree.max_size(); }
 
-  void swap(set<Key, Compare, Alloc> &x) { t.swap(x.t); }
+  void swap(set<Key, Compare, Alloc> &x) { _tree.swap(x._tree); }
 
   /*
    * modifiers
    */
-  pair<iterator, bool> insert(const value_type &x) { return t.insert_unique(x); }
+  pair<iterator, bool> insert(const value_type &x) { return _tree.insert_unique(x); }
   iterator insert(const_iterator position, const value_type &x) {
-    typedef typename _Rep_type::iterator _Rep_iterator;
-    return t.insert_unique((_Rep_iterator &)position, x);
+    typedef typename _Tree_type::iterator _Rep_iterator;
+    return _tree.insert_unique((_Rep_iterator &)position, x);
   }
   template <class InputIterator>
   void insert(InputIterator first, InputIterator last) {
-    t.insert_unique(first, last);
+    _tree.insert_unique(first, last);
   }
 
   void erase(const_iterator position) {
-    typedef typename _Rep_type::iterator _Rep_iterator;
-    t.erase((_Rep_iterator &)position);
+    typedef typename _Tree_type::iterator _Rep_iterator;
+    _tree.erase((_Rep_iterator &)position);
   }
-  size_type erase(const key_type &x) { return t.erase(x); }
+  size_type erase(const key_type &x) { return _tree.erase(x); }
   void erase(iterator first, iterator last) {
-    typedef typename _Rep_type::iterator _Rep_iterator;
-    t.erase((_Rep_iterator &)first, (_Rep_iterator &)last);
+    typedef typename _Tree_type::iterator _Rep_iterator;
+    _tree.erase((_Rep_iterator &)first, (_Rep_iterator &)last);
   }
-  void clear() { t.clear(); }
+  void clear() { _tree.clear(); }
 
   /*
    * observers
    */
-  allocator_type get_allocator() const { return t.get_allocator(); }
-  key_compare key_comp() const { return t.key_comp(); }
-  value_compare value_comp() const { return t.key_comp(); }
+  allocator_type get_allocator() const { return _tree.get_allocator(); }
+  key_compare key_comp() const { return _tree.key_comp(); }
+  value_compare value_comp() const { return _tree.key_comp(); }
 
-  // set operations
-  iterator find(const key_type &x) { return t.find(x); }
-  const_iterator find(const key_type &x) const { return t.find(x); }
-  size_type count(const key_type &x) const { return t.count(x); }
-  iterator lower_bound(const key_type &x) { return t.lower_bound(x); }
-  const_iterator lower_bound(const key_type &x) const { return t.lower_bound(x); }
-  iterator upper_bound(const key_type &x) { return t.upper_bound(x); }
-  const_iterator upper_bound(const key_type &x) const { return t.upper_bound(x); }
-  pair<iterator, iterator> equal_range(const key_type &x) { return t.equal_range(x); }
-  pair<const_iterator, const_iterator> equal_range(const key_type &x) const { return t.equal_range(x); }
+  /*
+   * lookup
+   */
+  iterator find(const key_type &x) { return _tree.find(x); }
+  const_iterator find(const key_type &x) const { return _tree.find(x); }
+  size_type count(const key_type &x) const { return _tree.count(x); }
+  iterator lower_bound(const key_type &x) { return _tree.lower_bound(x); }
+  const_iterator lower_bound(const key_type &x) const { return _tree.lower_bound(x); }
+  iterator upper_bound(const key_type &x) { return _tree.upper_bound(x); }
+  const_iterator upper_bound(const key_type &x) const { return _tree.upper_bound(x); }
+  pair<iterator, iterator> equal_range(const key_type &x) { return _tree.equal_range(x); }
+  pair<const_iterator, const_iterator> equal_range(const key_type &x) const { return _tree.equal_range(x); }
 
-  friend bool operator==(const set &x, const set &y) { return x.t == y.t; }
-  friend bool operator<(const set &x, const set &y) { return x.t < y.t; }
+  friend bool operator==(const set &x, const set &y) { return x._tree == y._tree; }
+  friend bool operator<(const set &x, const set &y) { return x._tree < y._tree; }
 };
 
 template <class Key, class Compare, class Alloc>
