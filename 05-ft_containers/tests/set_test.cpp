c086f40d42104fbe0a6e04364f070fc7a3873c29
junqwoopark <junqwoopark@gmail.com>
2023-01-24 13:34:11 +0900

    test 케이스 수정

--- 05-ft_containers/tests/set_test.cpp
+++ 05-ft_containers/tests/set_test.cpp
@@ -9,29 +9,29 @@ struct classcomp {
 int main() {
   // constructor
   {
-    std::set<int> first;  // empty set of ints
+    NAMESPACE::set<int> first;  // empty set of ints
 
     int myints[] = {10, 20, 30, 40, 50};
-    std::set<int> second(myints, myints + 5);  // range
+    NAMESPACE::set<int> second(myints, myints + 5);  // range
 
-    std::set<int> third(second);  // a copy of second
+    NAMESPACE::set<int> third(second);  // a copy of second
 
-    std::set<int> fourth(second.begin(), second.end());  // iterator ctor.
+    NAMESPACE::set<int> fourth(second.begin(), second.end());  // iterator ctor.
 
-    std::set<int, classcomp> fifth;  // class as Compare
+    NAMESPACE::set<int, classcomp> fifth;  // class as Compare
 
     bool (*fn_pt)(int, int) = fncomp;
-    std::set<int, bool (*)(int, int)> sixth(fn_pt);  // function pointer as Compare
+    NAMESPACE::set<int, bool (*)(int, int)> sixth(fn_pt);  // function pointer as Compare
   }
 
   // operator=
   {
     int myints[] = {12, 82, 37, 64, 15};
-    std::set<int> first(myints, myints + 5);  // set with 5 ints
-    std::set<int> second;                     // empty set
+    NAMESPACE::set<int> first(myints, myints + 5);  // set with 5 ints
+    NAMESPACE::set<int> second;                     // empty set
 
-    second = first;           // now second contains the 5 ints
-    first = std::set<int>();  // and first is empty
+    second = first;                 // now second contains the 5 ints
+    first = NAMESPACE::set<int>();  // and first is empty
 
     std::cout << "Size of first: " << int(first.size()) << '\n';
     std::cout << "Size of second: " << int(second.size()) << '\n';
@@ -40,10 +40,10 @@ int main() {
   // begin(), end()
   {
     int myints[] = {75, 23, 65, 42, 13};
-    std::set<int> myset(myints, myints + 5);
+    NAMESPACE::set<int> myset(myints, myints + 5);
 
     std::cout << "myset contains:";
-    for (std::set<int>::iterator it = myset.begin(); it != myset.end(); ++it) std::cout << ' ' << *it;
+    for (NAMESPACE::set<int>::iterator it = myset.begin(); it != myset.end(); ++it) std::cout << ' ' << *it;
 
     std::cout << '\n';
   }
@@ -51,9 +51,9 @@ int main() {
   // rbegin(), rend()
   {
     int myints[] = {21, 64, 17, 78, 49};
-    std::set<int> myset(myints, myints + 5);
+    NAMESPACE::set<int> myset(myints, myints + 5);
 
-    std::set<int>::reverse_iterator rit;
+    NAMESPACE::set<int>::reverse_iterator rit;
 
     std::cout << "myset contains:";
     for (rit = myset.rbegin(); rit != myset.rend(); ++rit) std::cout << ' ' << *rit;
@@ -63,7 +63,7 @@ int main() {
 
   // empty()
   {
-    std::set<int> myset;
+    NAMESPACE::set<int> myset;
 
     myset.insert(20);
     myset.insert(30);
@@ -79,7 +79,7 @@ int main() {
 
   // size()
   {
-    std::set<int> myints;
+    NAMESPACE::set<int> myints;
     std::cout << "0. size: " << myints.size() << '\n';
 
     for (int i = 0; i < 10; ++i) myints.insert(i);
@@ -95,7 +95,7 @@ int main() {
   // max_size()
   {
     int i;
-    std::set<int> myset;
+    NAMESPACE::set<int> myset;
 
     if (myset.max_size() > 1000) {
       for (i = 0; i < 1000; i++) myset.insert(i);
@@ -106,9 +106,9 @@ int main() {
 
   // insert()
   {
-    std::set<int> myset;
-    std::set<int>::iterator it;
-    std::pair<std::set<int>::iterator, bool> ret;
+    NAMESPACE::set<int> myset;
+    NAMESPACE::set<int>::iterator it;
+    std::pair<NAMESPACE::set<int>::iterator, bool> ret;
 
     // set some initial values:
     for (int i = 1; i <= 5; ++i) myset.insert(i * 10);  // set: 10 20 30 40 50
@@ -131,8 +131,8 @@ int main() {
 
   // erase()
   {
-    std::set<int> myset;
-    std::set<int>::iterator it;
+    NAMESPACE::set<int> myset;
+    NAMESPACE::set<int>::iterator it;
 
     // insert some values:
     for (int i = 1; i < 10; i++) myset.insert(i * 10);  // 10 20 30 40 50 60 70 80 90
@@ -155,30 +155,30 @@ int main() {
   // swap()
   {
     int myints[] = {12, 75, 10, 32, 20, 25};
-    std::set<int> first(myints, myints + 3);       // 10,12,75
-    std::set<int> second(myints + 3, myints + 6);  // 20,25,32
+    NAMESPACE::set<int> first(myints, myints + 3);       // 10,12,75
+    NAMESPACE::set<int> second(myints + 3, myints + 6);  // 20,25,32
 
     first.swap(second);
 
     std::cout << "first contains:";
-    for (std::set<int>::iterator it = first.begin(); it != first.end(); ++it) std::cout << ' ' << *it;
+    for (NAMESPACE::set<int>::iterator it = first.begin(); it != first.end(); ++it) std::cout << ' ' << *it;
     std::cout << '\n';
 
     std::cout << "second contains:";
-    for (std::set<int>::iterator it = second.begin(); it != second.end(); ++it) std::cout << ' ' << *it;
+    for (NAMESPACE::set<int>::iterator it = second.begin(); it != second.end(); ++it) std::cout << ' ' << *it;
     std::cout << '\n';
   }
 
   // clear()
   {
-    std::set<int> myset;
+    NAMESPACE::set<int> myset;
 
     myset.insert(100);
     myset.insert(200);
     myset.insert(300);
 
     std::cout << "myset contains:";
-    for (std::set<int>::iterator it = myset.begin(); it != myset.end(); ++it) std::cout << ' ' << *it;
+    for (NAMESPACE::set<int>::iterator it = myset.begin(); it != myset.end(); ++it) std::cout << ' ' << *it;
     std::cout << '\n';
 
     myset.clear();
@@ -186,23 +186,23 @@ int main() {
     myset.insert(2202);
 
     std::cout << "myset contains:";
-    for (std::set<int>::iterator it = myset.begin(); it != myset.end(); ++it) std::cout << ' ' << *it;
+    for (NAMESPACE::set<int>::iterator it = myset.begin(); it != myset.end(); ++it) std::cout << ' ' << *it;
     std::cout << '\n';
   }
 
   // key_comp()
   {
-    std::set<int> myset;
+    NAMESPACE::set<int> myset;
     int highest;
 
-    std::set<int>::key_compare mycomp = myset.key_comp();
+    NAMESPACE::set<int>::key_compare mycomp = myset.key_comp();
 
     for (int i = 0; i <= 5; i++) myset.insert(i);
 
     std::cout << "myset contains:";
 
     highest = *myset.rbegin();
-    std::set<int>::iterator it = myset.begin();
+    NAMESPACE::set<int>::iterator it = myset.begin();
     do {
       std::cout << ' ' << *it;
     } while (mycomp(*(++it), highest));
@@ -212,16 +212,16 @@ int main() {
 
   // value_comp()
   {
-    std::set<int> myset;
+    NAMESPACE::set<int> myset;
 
-    std::set<int>::value_compare mycomp = myset.value_comp();
+    NAMESPACE::set<int>::value_compare mycomp = myset.value_comp();
 
     for (int i = 0; i <= 5; i++) myset.insert(i);
 
     std::cout << "myset contains:";
 
     int highest = *myset.rbegin();
-    std::set<int>::iterator it = myset.begin();
+    NAMESPACE::set<int>::iterator it = myset.begin();
     do {
       std::cout << ' ' << *it;
     } while (mycomp(*(++it), highest));
@@ -231,8 +231,8 @@ int main() {
 
   // find()
   {
-    std::set<int> myset;
-    std::set<int>::iterator it;
+    NAMESPACE::set<int> myset;
+    NAMESPACE::set<int>::iterator it;
 
     // set some initial values:
     for (int i = 1; i <= 5; i++) myset.insert(i * 10);  // set: 10 20 30 40 50
@@ -248,7 +248,7 @@ int main() {
 
   // count()
   {
-    std::set<int> myset;
+    NAMESPACE::set<int> myset;
 
     // set some initial values:
     for (int i = 1; i < 5; ++i) myset.insert(i * 3);  // set: 3 6 9 12
@@ -264,8 +264,8 @@ int main() {
 
   // lower_bound(), upper_bound()
   {
-    std::set<int> myset;
-    std::set<int>::iterator itlow, itup;
+    NAMESPACE::set<int> myset;
+    NAMESPACE::set<int>::iterator itlow, itup;
 
     for (int i = 1; i < 10; i++) myset.insert(i * 10);  // 10 20 30 40 50 60 70 80 90
 
@@ -275,17 +275,17 @@ int main() {
     myset.erase(itlow, itup);  // 10 20 70 80 90
 
     std::cout << "myset contains:";
-    for (std::set<int>::iterator it = myset.begin(); it != myset.end(); ++it) std::cout << ' ' << *it;
+    for (NAMESPACE::set<int>::iterator it = myset.begin(); it != myset.end(); ++it) std::cout << ' ' << *it;
     std::cout << '\n';
   }
 
   // equal_range()
   {
-    std::set<int> myset;
+    NAMESPACE::set<int> myset;
 
     for (int i = 1; i <= 5; i++) myset.insert(i * 10);  // myset: 10 20 30 40 50
 
-    std::pair<std::set<int>::const_iterator, std::set<int>::const_iterator> ret;
+    std::pair<NAMESPACE::set<int>::const_iterator, NAMESPACE::set<int>::const_iterator> ret;
     ret = myset.equal_range(30);
 
     std::cout << "the lower bound points to: " << *ret.first << '\n';
