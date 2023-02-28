b79156fd69046daf546c3753be7c9d4701ae6a0e
Junkyu Park <junqwoopark@gmail.com>
2023-02-28 22:30:48 +0900

    nginx ssl, fastcgi 설정 수정

--- 05-inception/srcs/requirements/nginx/tools/ssl-keygen.sh
+++ 05-inception/srcs/requirements/nginx/tools/ssl-keygen.sh
@@ -4,6 +4,6 @@ set -e
 
 mkdir -p /etc/nginx/ssl
 
-if [ ! -e /etc/nginx/ssl/nginx-ssl.crt ]; then
-    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx-ssl.key -out /etc/nginx/ssl/nginx-ssl.crt -subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/OU=JunkPark/CN=junkpark.42.fr"
+if [ ! -e /etc/nginx/ssl/junkpark.42.fr.crt ]; then
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/junkpark.42.fr.key -out /etc/nginx/ssl/junkpark.42.fr.crt -subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/OU=JunkPark/CN=junkpark.42.fr"
 fi
