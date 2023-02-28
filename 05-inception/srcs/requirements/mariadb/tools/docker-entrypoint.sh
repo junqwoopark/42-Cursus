7beef5ddfcbd77428457dec034bf63150bc9b7bc
Junkyu Park <junqwoopark@gmail.com>
2023-02-28 22:29:07 +0900

    Access denied for user 'root'@'localhost' (using password: NO) 문제 해결

--- 05-inception/srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ 05-inception/srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -2,21 +2,15 @@
 
 set -e
 
-MYSQL_SETUP_FILE=/var/lib/mysql/.setup
-
 service mysql start
 
-if [ ! -e $MYSQL_SETUP_FILE ]; then
+if [ ! -e /var/lib/mysql/$MYSQL_DATABASE ]; then
 	mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE";
 	mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'";
-	mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%'";
-	mysql -e "FLUSH PRIVILEGES";
-	mysql -e "ALTER USER '$MYSQL_ROOT'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'";
-
-	mysql $MYSQL_DATABASE -u$MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD
-	mysqladmin -u$MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD shutdown
-
-	touch $MYSQL_SETUP_FILE
+	mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%'; FLUSH PRIVILEGES;"
+	mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;"
 fi
 
-exec mysqld_safe
+mysqladmin -u$MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD shutdown
+
+exec "mysqld_safe"
