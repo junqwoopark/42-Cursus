34ab6de7a2248cc1740ceaf27fd85ae8dacf4cd1
Junkyu Park <junqwoopark@gmail.com>
2023-03-04 23:54:01 +0900

    feat: ftp container 추가

---
+++ 05-inception/srcs_bonus/requirements/mariadb/tools/docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+service mysql start
+
+if [ ! -e /var/lib/mysql/$MYSQL_DATABASE ]; then
+	mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE";
+	mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'";
+	mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%'; FLUSH PRIVILEGES;"
+	mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;"
+fi
+
+mysqladmin -u$MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD shutdown
+
+exec "mysqld_safe"
