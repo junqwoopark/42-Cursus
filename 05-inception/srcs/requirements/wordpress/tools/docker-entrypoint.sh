e8881b630d6134fcc171ff6a19f5ec0546be335d
Junkyu Park <junqwoopark@gmail.com>
2023-03-26 22:07:43 +0900

    feat: wordpress 일반 유저 추가

--- 05-inception/srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ 05-inception/srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -10,8 +10,9 @@ if [ ! -f /var/www/html/index.php ]; then
   chmod +x wp-cli.phar
   mv wp-cli.phar /usr/local/bin/wp
   wp core download --allow-root
-  p config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --allow-root
-  wp core install --url=https://junkpark.42.fr --title="JunkPark" --admin_user=$WP_USER --admin_password=$WP_PASSWORD --admin_email=$WP_EMAIL --skip-email --allow-root
+  wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --allow-root
+  wp core install --url=https://junkpark.42.fr --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+  wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --role=author --allow-root
 fi
 
 exec "php-fpm7.3" "-F"
