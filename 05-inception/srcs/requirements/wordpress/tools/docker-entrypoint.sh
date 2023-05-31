182a79b861cd2dddc1e6bb6ed4bde86739868daf
junqwoopark <junqwoopark@gmail.com>
2023-05-31 11:08:39 +0900

    chore: .gitkeep 삭제, WP_LOCALE 추가, 주석 수정 등

--- 05-inception/srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ 05-inception/srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -8,7 +8,7 @@ if [ ! -f /var/www/html/index.php ]; then
   wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
   chmod +x wp-cli.phar
   mv wp-cli.phar /usr/local/bin/wp
-  wp core download --allow-root
+  wp core download --locale=$WP_LOCALE --allow-root
   wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --allow-root
   wp core install --url=https://junkpark.42.fr --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
   wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --role=author --allow-root
